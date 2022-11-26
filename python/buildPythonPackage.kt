113158fae58265469e4c97028306db7797ea4264
Laurent Michel <laurent.michel@cognex.com>
2022-11-26 16:27:18 +0100

    add creds before building project

--- python/buildPythonPackage.kt
+++ python/buildPythonPackage.kt
@@ -10,6 +10,7 @@ fun BuildSteps.buildPythonPackage(dockerToolsTag: String): ScriptBuildStep {
         scriptContent = """
                 #! /bin/sh
 
+                poetry config http-basic.pypi-group %system.package-manager.deployer.username% %system.package-manager.deployer.password%
                 poetry build
             """.trimIndent()
         this.dockerImage = "%system.docker-registry.group%/docker-tools/poetry:$dockerToolsTag"
