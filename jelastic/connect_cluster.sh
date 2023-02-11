4eed00c46061510ab010a97f4a0c004133ffc1f0
Laurent Michel <laurent.michel@cognex.com>
2023-02-11 15:09:57 +0100

    simplify k8s cluster connection

--- jelastic/connect_cluster.sh
+++ jelastic/connect_cluster.sh
@@ -1,9 +1,10 @@
 #! /bin/sh
 
-CLUSTER_USERNAME=${KUBERNETES_CLUSTER_NAME}
+CLUSTER_NAME=jelastic-cluster
+CLUSTER_USERNAME=${CLUSTER_NAME}
 
-kubectl config set-cluster "${KUBERNETES_CLUSTER_NAME}" --server="${KUBERNETES_API_URL}"
-kubectl config set-context "${KUBERNETES_CLUSTER_NAME}" --cluster="${KUBERNETES_CLUSTER_NAME}"
+kubectl config set-cluster "${CLUSTER_NAME}" --server="${KUBERNETES_API_URL}"
+kubectl config set-context "${CLUSTER_NAME}" --cluster="${CLUSTER_NAME}"
 kubectl config set-credentials "${CLUSTER_USERNAME}" --token="${KUBERNETES_API_TOKEN}"
-kubectl config set-context "${KUBERNETES_CLUSTER_NAME}" --user="${CLUSTER_USERNAME}"
-kubectl config use-context "${KUBERNETES_CLUSTER_NAME}"
+kubectl config set-context "${CLUSTER_NAME}" --user="${CLUSTER_USERNAME}"
+kubectl config use-context "${CLUSTER_NAME}"
